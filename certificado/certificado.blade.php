9fe7f2c7b1858205705ffc22fec962c9fcb56290
Danny Yuliana Aguilar Soto <80791954+11dany@users.noreply.github.com>
2021-04-21 11:14:49 -0500

    Add files via upload

---
+++ certificado/certificado.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>certificado de votacion</title> 
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header class="logo">
+        <div class="logo_img">
+            <img src="imagenes/logo sena.jpg" alt="logo" class="logo_img"> <br>
+            <h4 class="nom_logo">REGIONAL CAQUETA</h4> 
+
+        </div>
+        
+
+    </header>
+    <div class="container">
+        <h2 class="titulo_cont">CENTRO TECNOLOGICO DE LA AMAZONIA</h2>
+        <h3 class="sub_titulo">CERTIFICA</h3>
+        <p class="parrafo_cont"><em>Que el aprendiz juanito perez con cedula de ciudadania No. 1006504964 relizo correctamente la votacion para la elección de representantes de los aprendices SENA</em></p> <br>
+        <p class="parrafo_cont"><em>Se expide en Florencia. a los (4) dias del mes de abril del presente año </em></p> <br>
+        <p class="firma"><strong>Firma digital por</strong></p> <br>
+          
+    </div>
+
+   
+
+   
+</body>
+</html>
